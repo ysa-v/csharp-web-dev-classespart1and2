953fd2d21ff2a953f63f481449a08f07d1bfc246
ysa-v <ysa.vandenberg@gmail.com>
2022-08-17 16:41:03 -0500

    Add classes for course and teacher

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,14 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+            Student myself = new Student("Ysabel", 000782338, 1, 4.0);
+
+            Console.WriteLine("The student's name is " + myself.Name + "!");
+
+            myself.Name = "still Ysabel";
+
+            Console.WriteLine("The student's name is " + myself.Name + "!");
         }
     }
 }
