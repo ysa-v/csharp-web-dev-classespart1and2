684b5a77f80459ee8552dbe4d3955c99c7e9ed05
ysa-v <ysa.vandenberg@gmail.com>
2022-08-24 13:19:26 -0500

    Added methods for grade level, gpa, ToString and Equals

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -19,5 +19,56 @@ namespace SchoolPractice
             NumberOfCredits = numberOfCredits;
             Gpa = gpa;
         }
+
+        public string GetGradeLevel(int credits)
+        {
+            if (credits > 30)
+            {
+                return 'freshman';
+            }
+            else if (credits > 60)
+            {
+                return 'sophomore';
+            }
+            else if (credits > 90)
+            {
+                return 'junior';
+            }
+            else
+            {
+                return 'senior';
+            }
+        }
+
+        public void AddGrade(int credits, double grade)
+        {
+            double totalQualityScore = Gpa * NumberOfCredits;
+
+            Gpa = totalQualityScore + grade / NumberOfCredits + credits;
+        }
+
+        public override string ToString()
+        {
+            return Name + " (Credits: " + NumberOfCredits + ", GPA: " + Gpa + ")";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+            {
+                return true;
+            }
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Student studentObj = obj as Student;
+            return StudentId == studentObj.StudentId;
+        }
     }
 }
